e64d37457ae24ba87eaee65b239af89b4beca881
kaoru <k@warpnine.io>
2024-12-29 14:54:48 +0900

    feat: shell auto completion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::path::PathBuf;
+use std::{fmt, fmt::Display, path::PathBuf, str::FromStr};
 
 use bo::BookmarkManager;
 use clap::Parser;
+use tokio::{
+    fs::File,
+    io::{AsyncWriteExt, BufWriter},
+};
 
 #[derive(Debug, Parser)]
 #[clap(version)]
@@ -16,16 +20,106 @@ pub struct Args {
     /// Path to the configuration file. Defaults to $XDG_CONFIG_HOME/bo/config.toml.
     #[arg(short, long)]
     pub config: Option<PathBuf>,
+
+    #[clap(subcommand)]
+    pub command: Option<Command>,
+}
+
+#[derive(Debug, Parser)]
+pub enum Command {
+    /// Generate a shell completion script. At the moment, only `fish` is supported.
+    GenerateCompletion {
+        /// The shell to generate completion scripts for.
+        #[arg(short, long, default_value = "fish")]
+        shell: Shell,
+
+        /// The path to write the completion script to.
+        #[arg(short, long, default_value = "~/.config/fish/completions/bo.fish")]
+        path: String,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub enum Shell {
+    Fish,
+}
+
+impl Display for Shell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Fish => write!(f, "fish"),
+        }
+    }
+}
+
+impl FromStr for Shell {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        match s {
+            "fish" => Ok(Self::Fish),
+            _ => Err(anyhow::anyhow!("Unsupported shell: {s}")),
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let Args { args, config } = Args::parse();
+    let Args { args, config, command } = Args::parse();
     let manager = BookmarkManager::from(config).await?;
 
-    match args {
-        Some(args) if args.len() == 1 => manager.open(args.first().unwrap()),
-        Some(args) if args.len() > 1 => manager.search(&args),
-        _ => manager.open_prompt(),
+    match command {
+        Some(Command::GenerateCompletion { shell: _, path }) => {
+            generate_fish_completion(&manager, &path).await
+        }
+        None => match args {
+            Some(args) if args.len() == 1 => manager.open(args.first().unwrap()),
+            Some(args) if args.len() > 1 => manager.search(&args),
+            _ => manager.open_prompt(),
+        },
     }
+}
+
+async fn generate_fish_completion(manager: &BookmarkManager, path: &str) -> anyhow::Result<()> {
+    let file = PathBuf::from(shellexpand::tilde(&path).to_string());
+    let mut out = BufWriter::new(File::create(&file).await?);
+
+    out.write_all(b"# fish shell completions for bo command\n").await?;
+    out.write_all(b"complete -c bo -f -n \"not __fish_seen_subcommand_from completion\"\n")
+        .await?;
+    out.write_all(
+        b"complete -c bo -f -n \"not __fish_seen_subcommand_from completion\" -a generate-completion -d \"command: Generate shell completion script\"\n",
+    )
+        .await?;
+
+    for (name, url_config) in manager.bookmarks.iter() {
+        out.write_all(
+            format!(
+                "complete -c bo -f -n \"not __fish_seen_subcommand_from completion\" -a \"{}\" -d \"{}\"\n",
+                name,
+                url_config.url.replace("https://", "").replace("http://", "").replace("www.", ""),
+            )
+                .as_bytes(),
+        )
+            .await?;
+    }
+
+    if let Some(aliases) = manager.aliases.as_ref() {
+        for (alias, name) in aliases.iter() {
+            out.write_all(
+                format!(
+                    "complete -c bo -f -n \"not __fish_seen_subcommand_from completion\" -a \"{}\" -d \"{}\"\n",
+                    alias,
+                    manager.bookmarks.get(name).unwrap().url.replace("https://", "").replace("http://", "").replace("www.", ""),
+                )
+                    .as_bytes(),
+            )
+                .await?;
+        }
+    }
+
+    out.flush().await?;
+    println!("Auto completion file updated: {}", file.display());
+
+    Ok(())
 }
